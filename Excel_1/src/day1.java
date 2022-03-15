25707a6318a6391f76555fbf0bb6f2b7bca3bb91
admin <admin@LAPTOP-F6MR520K>
2022-03-15 13:29:31 +0530

    Excel sheet new code

---
+++ Excel_1/src/day1.java
@@ -0,0 +1,32 @@
+import java.io.FileInputStream;
+
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class day1 {
+
+	
+        public static void main(String[] args) throws EncryptedDocumentException, IOException {
+	
+
+		
+        String path = "C:\\Users\\admin\\Desktop\\Book3.xltx";
+		
+		FileInputStream file = new FileInputStream(path);
+		
+		String data = WorkbookFactory.create(file).getSheet("Sheet1").getRow(3).getCell(3).getStringCellValue();
+		System.out.println(data);
+
+	
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
